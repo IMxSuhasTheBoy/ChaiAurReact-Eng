47bec07fe94c53f8d16db0b98c472faec6e9f0d2
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-12-09 20:12:00 +0530

    05 Props, useState hook usage

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -3,9 +3,42 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import TestComponent from "./TestComponent.jsx";
 
+const myReactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom react app</h1>
+    </div>
+  );
+}
+
+const TestDirectUse = (
+  <a href="https://google.com" target="_blank">
+    Vistosss
+  </a>
+);
+
+const Actual_ReactElement_Object_BreakDown = React.createElement(
+  "a", ///type
+
+  { href: "https://google.com", target: "_blank" }, ///attributes
+
+  "Made with React.createElement", ///children
+
+  " " //{exp of JSX comp...}//this 4th object comes when theres evaluated expressions are recieved, NOTE: Its for rendering purpose (But expressions that is to be evaluated are not acceptable for rendering) empty string as 4th object can be your bug prevention strat.
+); // console.log(Actual_ReactElement_Object_BreakDown)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-    <TestComponent />
-  </>
+  // TestDirectUse
+  // Actual_ReactElement_Object_BreakDown
+
+  <App />
 );
---
+++ customReact/customreact.js
@@ -0,0 +1,24 @@
+function customRenderMethod(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+
+  for (const prop in reactElement.props) {
+    if (prop === "children") continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
+///
+customRenderMethod(reactElement, mainContainer);
+///using customRenderMethod( **passing what to inject, where to inject** )
