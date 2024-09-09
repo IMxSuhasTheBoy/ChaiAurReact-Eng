66eaeeefd7f16e3a86c75d9cd6576e962e0604fd
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-09-09 12:33:42 +0530

    Add changes
    Explored the rendering proccess of react by experimenting with it & mimic it with javascript & html DOM manipulation

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,14 +1,20 @@
+// 3: create a custom render method
 function customRenderMethod(reactElement, container) {
+  // 3.1: create a dom element (type, children)
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
 
+  // 3.2: set attributes to the dom element
   for (const prop in reactElement.props) {
     if (prop === "children") continue;
     domElement.setAttribute(prop, reactElement.props[prop]);
   }
+
+  // 3.3: append the dom element to the container
   container.appendChild(domElement);
 }
 
+// 1: create a react element data structure
 const reactElement = {
   type: "a",
   props: {
@@ -18,7 +24,9 @@ const reactElement = {
   children: "Click me to visit google",
 };
 
+// 2: select the root element (where to inject)
 const mainContainer = document.querySelector("#root");
-///
+
+// 4: use the customRenderMethod to inject the reactElement into the DOM (what to inject)
 customRenderMethod(reactElement, mainContainer);
-///using customRenderMethod( **passing what to inject, where to inject** )
+// using customRenderMethod( **passing what to inject, where to inject** )
