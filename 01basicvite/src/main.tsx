4b253093ea2ccd80a58ac8590c2ae524f4efa8ad
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-09-09 12:29:43 +0530

    Add code- react rendering tests
    Explored react inner workings of rendering method by experimenting it by rendering custom objects & variables.

--- 01basicvite/src/main.tsx
+++ 01basicvite/src/main.tsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 // import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+/*
+const customReactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+*/
+
+/*
+const AnotherCustomElement = (
+  <a href="https://google.com" target="_blank">
+    |Click me to visit google|
+  </a>
+);
+*/
+
+/*
+// things passed through createElement are then converted to a object(expected by react) to loop onto to finally render
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Visit google",
+  AnotherCustomElement
+);
+*/
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+//
+/*
+createRoot(document.getElementById("root")!).render(
+  // AnotherCustomElement
+  reactElement
+);
+*/
